82e7922417762d0da320600c72fa059941882f99
JeremiahS13 <148928125+realhahatoo@users.noreply.github.com>
2025-03-08 21:09:13 +0000

    Finished Changing Pictures

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
 function fix() {
-    //Write your code here!
+    let coconut = document.getElementById("coconut");
+    coconut.remove()
+    let coast = document.getElementById("coast");
+    coast.remove()
+    let watermelon = document.getElementById("watermelon");
+    watermelon.remove()
+    let sunflowers = document.getElementById("sunflowers");
+    sunflowers.remove()
+
+
+    let tree = document.createElement("img");
+    tree.setAttribute ("id", "tree") 
+    tree.setAttribute("src", "to-add/christmasTree.jpg")
+    let snowmen = document.createElement("img");
+    snowmen.setAttribute ("id", "snowmen") 
+    snowmen.setAttribute("src", "to-add/snowmen.jpg")
+    let stockings = document.createElement("img");
+    stockings.setAttribute ("id", "stockings") 
+    stockings.setAttribute("src", "to-add/stockings.jpg")
+
+
+    let photos = document.getElementById("photos");
+    photos.appendChild(tree);
+    photos.appendChild(snowmen);
+    photos.appendChild(stockings);
 }
